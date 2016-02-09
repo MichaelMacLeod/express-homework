078956006adc3308a52e25c13be634450e5d2a11
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-09 07:48:52 +0000

    Added models directory and record_store.js file for the data

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send("Welcome to the Record Store");
+  res.render('index', { welcome: "Welcome to the Record Store" });
 });
+
+
 
 app.listen('3000', function(){
   console.log('Record Store running on port 3000');
 })
+
