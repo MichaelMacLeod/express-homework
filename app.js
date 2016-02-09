3db6a0f5c6269c4a6e3a564e5988f8bcbd043e46
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-09 07:42:17 +0000

    Added routes, served on local host, installed nodemon and message is logged on local host 3000

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var express = require('express');
 var app = express();
+
+app.get('/', function(req, res) {
+  res.send("Welcome to the Record Store");
+});
+
+app.listen('3000', function(){
+  console.log('Record Store running on port 3000');
+})
