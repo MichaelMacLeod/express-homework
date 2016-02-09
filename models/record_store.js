6f758968a3b6615344baed82f36c79dd3888dfe4
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-09 07:51:46 +0000

    Added three records to record_store.js object

---
+++ models/record_store.js
@@ -0,0 +1,22 @@
+var RecordStore = {
+  "albums": [
+  {
+    "album": "Brave Gravity",
+    "artist": "Jetplane Landing"
+  }, 
+  {
+    "album": "Silver Wilkinson",
+    "artist": "Bibio"
+  },
+  {
+    "album": "Debut",
+    "artist": "Bjork"
+  }
+  ]
+}
+
+module.exports = RecordStore;
+
+
+
+
